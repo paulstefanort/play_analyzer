2953c8e5edb16ebef1c9a20f2468e7dc6312fd6a
Paul Stefan Ort <mail@paulstefanort.com>
2014-08-19 23:55:03 -0500

    Update presentation.

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -2,17 +2,22 @@ require 'open-uri'
 require 'analyzer'
 
 class SiteController < ApplicationController
+  before_filter :load_plays
+
   def index
-    @plays = Play.list
   end
 
   def play
-    slug = params[:slug]
-    @play = Play.list.select { |play| play[:slug] == slug }.first
+    @play = Play.list.select { |play| play[:slug] == params[:slug] }.first
     file_name = @play[:file]
     file_path = File.expand_path("../../../data", __FILE__)
     @file = open("#{file_path}/#{file_name}")
     analyzer = Analyzer.new(file: @file)
     @speakers = analyzer.analyze_speakers
+  end
+
+  private
+  def load_plays
+    @plays = Play.list
   end
 end
